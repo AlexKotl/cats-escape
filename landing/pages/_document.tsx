71ca78838723c79f416ec255a66391952894189b
Oleksandr Kotliarov <oleksandrkotliarov@marc-o-polo.com>
2024-07-04 11:08:21 +0200

    Add seo title

--- landing/pages/_document.tsx
+++ landing/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html data-theme="night">
-      <Head />
+      <Head>
+        <title>Cats Escape the Game</title>
+        <meta
+          name="description"
+          content="The game about lazy cats and dumb mouse."
+        />
+      </Head>
       <body className="flex justify-center items-center min-h-screen bg-base-200">
         <Main />
         <NextScript />
